b5a8ca75b0765d6715d21511649b1da74dd22d01
Toan Nguyen <toan2406@gmail.com>
2016-07-04 15:10:31 +0700

    add sample solution

---
+++ values-types-and-operators/numbers.js
@@ -0,0 +1,9 @@
+var annualLeave = 15
+var sickLeave = 15
+var traningLeave = 10
+var sum = annualLeave + sickLeave + traningLeave
+
+exports.annualLeave = annualLeave
+exports.sickLeave = sickLeave
+exports.traningLeave = traningLeave
+exports.sum = sum
