707d739fd69267d7658f1eb98a7b2e4870db93c0
Toan Nguyen <toan2406@gmail.com>
2016-10-06 00:02:45 +0700

    Update conditional-execution.js

--- program-structure/conditional-execution.js
+++ program-structure/conditional-execution.js
@@ -1,12 +1,8 @@
 /*
- * Bob
- * Bob is a lackadaisical teenager. In conversation, his responses are very limited.
- * Bob answers 'Sure.' if you ask him a question.
- * He answers 'Whoa, chill out!' if you yell at him.
- * He says 'Fine. Be that way!' if you address him without actually saying anything.
- * He answers 'Whatever.' to anything else.
+ * Hello World
+ * Write a function that greets the user by name, or by saying "Hello, World!" if no name is given.
  */
 
-exports.heyBob = function (words) {
-
+exports.hello = function (name) {
+  your_answer
 }
