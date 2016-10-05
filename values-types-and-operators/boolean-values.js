23f6773ab5d4150e8a8a105681c6bf0f0212df4b
Toan Nguyen <toan2406@gmail.com>
2016-10-05 23:49:30 +0700

    Update boolean-values.js

--- values-types-and-operators/boolean-values.js
+++ values-types-and-operators/boolean-values.js
@@ -1,7 +1,7 @@
 /*
  * Ranking
  * Write a program that ranks student's grade
- *  if grade is greater than or equals 60, return "Average"
+ *  if grade is less than 60, return "Average"
  *  if grade is greater than or equals 60, and less than 90, return "Good"
  *  else return "Excellent"
  */
