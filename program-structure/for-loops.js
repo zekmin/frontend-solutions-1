8110d0f6d39f58d478d2d6828d73e5915ddabedf
Toan Nguyen <toan2406@gmail.com>
2016-10-06 18:53:50 +0700

    Update for-loops.js

--- program-structure/for-loops.js
+++ program-structure/for-loops.js
@@ -1,6 +1,6 @@
 /*
  * FizzBuzz
- * Write a program that uses console.log to print all the numbers from 1 to 100, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
+ * Write a program that uses console.log to print all the numbers from 0 to 99, with two exceptions. For numbers divisible by 3, print "Fizz" instead of the number, and for numbers divisible by 5 (and not 3), print "Buzz" instead.
  */
 
 exports.fizzBuzz = your_answer
